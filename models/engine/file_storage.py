db98d0c3a76a870f11ec39886a067fce46f2f71a
Sam <97474996+bashthat@users.noreply.github.com>
2022-07-01 13:59:38 -0500

    did the import raise the flag?

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+import os
+import datetime
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
