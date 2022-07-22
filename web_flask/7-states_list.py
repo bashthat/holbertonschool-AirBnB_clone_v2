81c8b12270aa4244826d8308b52e8b1538c67c8b
bashthat <3249@holbertonschool.com>
2022-07-21 21:06:30 -0500

    less is more

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """app route states_list, import storage.all, display html states in <h1> header """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    """app route states_list """
+    states = (storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
