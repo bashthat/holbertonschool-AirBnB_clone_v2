b46ab9211c48c12ea044a94c1aa1a19b0de0598c
bashthat <3249@holbertonschool.com>
2022-07-21 21:07:00 -0500

    pep

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,7 +18,7 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
+@ app.teardown_appcontext
 def teardown_db(exception):
     """ removing SQLAlchemy """
     storage.close()
